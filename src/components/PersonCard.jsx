fd4d7da1803ddb17afe8535bb28d4b3ba291ae52
Colette Ulianogit config --global --listclear <culiano@live.com>
2023-02-14 18:16:52 -0500

    update file

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
+    const [age, setAge] = useState(props.age);
+
+    const buttonClick = () => {
+        setAge(age + 1);
+    }
+
     return (
         <div className='card m-3 p-3'>
             <h2>{props.lastName}, {props.firstName}</h2>
-            <p>Age: {props.age}</p>
+            <p>Age: {age}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button className='btn btn-primary w-50' onClick={buttonClick}>Birthday Button for {props.firstName}</button>
         </div>
     )
 }
